c868c3c09c291fd3b7eda1df0ec5998dd4a837be
songweishuai <240948380@qq.com>
2020-08-13 15:49:05 +0800

    增加摄像头属性设置

--- CameraSurface/app/src/main/java/com/example/camerasurface/MainActivity.java
+++ CameraSurface/app/src/main/java/com/example/camerasurface/MainActivity.java
@@ -4,29 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Camera;
 import android.hardware.camera2.CameraDevice;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     SurfaceView surfaceView;
     SurfaceHolder holder;
     Camera mCamera;
+    private String TAG = "Surface Camera Demo";
 
-//    Camera mCamera_1;
-//    SurfaceView surfaceView_1;
-//    SurfaceHolder holder_1;
+    //Button mButton;
+    //Camera mCamera_1;
+    //SurfaceView surfaceView_1;
+    //SurfaceHolder holder_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //mButton = findViewById(R.id.button);
+        //mButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+        //
+        //        if (mCamera != null) {
+        //            mCamera.stopPreview();
+        //            mCamera.release();
+        //        }
+        //
+        //        mCamera = Camera.open(1);
+        //        if (mCamera == null) {
+        //            Log.i("onResume", "mCamera is null ");
+        //        }
+        //
+        //        //相机参数设置
+        //        Camera.Parameters param = mCamera.getParameters();
+        //        param.setPreviewSize(640, 480);
+        //        mCamera.setParameters(param);
+        //
+        //        mCamera.startPreview();
+        //    }
+        //});
+
         surfaceView = findViewById(R.id.surfaceView);
         holder = surfaceView.getHolder();
+
         holder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
@@ -50,30 +83,31 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-//        surfaceView_1 = findViewById(R.id.surfaceView_1);
-//        holder_1 = surfaceView_1.getHolder();
-//        holder_1.addCallback(new SurfaceHolder.Callback() {
-//            @Override
-//            public void surfaceCreated(SurfaceHolder holder) {
-//
-//            }
-//
-//            @Override
-//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-//                try {
-//                    mCamera_1.setPreviewDisplay(holder);
-//                    mCamera_1.startPreview();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void surfaceDestroyed(SurfaceHolder holder) {
-//
-//            }
-//
-//        });
+        //surfaceView_1 = findViewById(R.id.surfaceView_1);
+        //holder_1 = surfaceView_1.getHolder();
+        //holder_1.addCallback(new SurfaceHolder.Callback() {
+        //    @Override
+        //    public void surfaceCreated(SurfaceHolder holder) {
+        //
+        //    }
+        //
+        //    @Override
+        //    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        //        try {
+        //            mCamera_1.setPreviewDisplay(holder);
+        //            mCamera_1.startPreview();
+        //        } catch (IOException e) {
+        //            e.printStackTrace();
+        //        }
+        //    }
+        //
+        //    @Override
+        //    public void surfaceDestroyed(SurfaceHolder holder) {
+        //
+        //    }
+        //
+        //});
+
     }
 
 
@@ -83,39 +117,58 @@ public class MainActivity extends AppCompatActivity {
 
         int cout = Camera.getNumberOfCameras();
         int CameraIndex = 0;
-        Log.i("***************", "Camera Count:" + cout);
+        Log.i(TAG, "Camera Count:" + cout);
         Camera.CameraInfo info = new Camera.CameraInfo();
 
 
         for (int nIndex = 0; nIndex < cout; nIndex++) {
             Camera.getCameraInfo(nIndex, info);
 
-            Log.i("***************", "info.orientation:" + info.orientation);
-            Log.i("***************", "info.facing" + info.facing);
+            Log.i(TAG, "info.orientation:" + info.orientation);
+            Log.i(TAG, "info.facing" + info.facing);
 
             CameraIndex = nIndex;
         }
 
-        Log.i("***************", "CameraIndex:" + CameraIndex);
+        Log.i(TAG, "CameraIndex:" + CameraIndex);
 
-        mCamera = Camera.open(0);
+        mCamera = Camera.open(1);
         if (mCamera == null) {
-            Log.i("onResume", "mCamera is null ");
+            Log.i(TAG, "mCamera is null ");
         }
+
+        //相机参数设置
         Camera.Parameters param = mCamera.getParameters();
-        //param.setPictureSize(640, 480);
+
+        //获取支持分辨率
+        List<Camera.Size> supportSize = param.getSupportedPreviewSizes();
+        for (Camera.Size size : supportSize) {
+            Log.i(TAG, "Support Size w:" + size.width + "  h:" + size.height);
+        }
+
+        //设置视频分辨率
         param.setPreviewSize(640, 480);
-        //param.setPreviewFpsRange(15, 30);
+
+        //自动对焦
+        List<String> focusMode = param.getSupportedFocusModes();
+        Log.i(TAG, "focusMode:" + focusMode);
+        if(!focusMode.contains(Camera.Parameters.FOCUS_MODE_AUTO)){
+            Log.i(TAG, "focusMode 不支持自动对焦");
+        }
+
+        //设置参数
         mCamera.setParameters(param);
 
-        //mCamera.setDisplayOrientation(90);
+        //设置旋转
+        mCamera.setDisplayOrientation(270);
 
-//        mCamera_1 = Camera.open(1);
-//        if (mCamera_1 == null) {
-//            Log.i("onResume", "mCamera_1 is null ");
-//        }
-//        param.setPreviewSize(640, 480);
-//        mCamera_1.setParameters(param);
+        //mCamera_1 = Camera.open(1);
+        //if (mCamera_1 == null) {
+        //  Log.i("onResume", "mCamera_1 is null ");
+        //}
+        //param.setPreviewSize(640, 480);
+        //mCamera_1.setParameters(param);
+
     }
 
     @Override
@@ -126,10 +179,10 @@ public class MainActivity extends AppCompatActivity {
             mCamera.release();
         }
 
-//        if (mCamera_1 != null) {
-//            mCamera_1.stopPreview();
-//            mCamera_1.release();
-//        }
+        //if (mCamera_1 != null) {
+        //  mCamera_1.stopPreview();
+        //  mCamera_1.release();
+        //}
     }
 
     @Override
@@ -140,9 +193,9 @@ public class MainActivity extends AppCompatActivity {
             mCamera.release();
         }
 
-//        if (mCamera_1 != null) {
-//            mCamera_1.stopPreview();
-//            mCamera_1.release();
-//        }
+        //if (mCamera_1 != null) {
+        //mCamera_1.stopPreview();
+        //mCamera_1.release();
+        //}
     }
 }
